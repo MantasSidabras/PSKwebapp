5c96d9156df596c676842a5860eae3520b14cf0c
MantasSidabras <sid.mantas@gmail.com0>
2018-05-02 03:39:47 +0300

    working batis

---
+++ src/main/java/persistence/BooksDAO.java
@@ -0,0 +1,18 @@
+package persistence;
+
+
+import entities.Book;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@ApplicationScoped
+public class BooksDAO {
+    @Inject
+    private EntityManager em;
+
+    public void save(Book book) {
+        this.em.persist(book);
+    }
+}
--- src/main/java/usecases/Authors.java
+++ src/main/java/usecases/Authors.java
@@ -1,31 +1,46 @@
 package usecases;
 
 import entities.Author;
+import persistence.AuthorsDAO;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
-import java.util.ArrayList;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Model
 public class Authors {
+    @Inject
+    private AuthorsDAO authorsDAO;
     private List<Author> allAuthors;
+    //Default initial values could be set here or inside the Author model - which is better?
+    private Author newAuthor = new Author();
 
     @PostConstruct
     public void init() {
         this.loadAuthors();
     }
 
-    private void loadAuthors() {
-        // TODO this is a mock implementation - later we will connect it to real data store
-        List<Author> authors = new ArrayList<Author>();
-        authors.add(new Author("Author1"));
-        authors.add(new Author("Author2"));
-        this.allAuthors = authors;
+    @Transactional
+    public String createNewAuthor() {
+        authorsDAO.save(newAuthor);
+        return "index?faces-redirect=true";
     }
 
+    private void loadAuthors() {
+        this.allAuthors = authorsDAO.loadAll();
+    }
 
     public List<Author> getAllAuthors() {
         return allAuthors;
+    }
+
+    public Author getNewAuthor() {
+        return newAuthor;
+    }
+
+    public void setNewAuthor(Author newAuthor) {
+        this.newAuthor = newAuthor;
     }
 }
